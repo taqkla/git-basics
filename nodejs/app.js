d9a93968295b20f20d866545e7ce5620d2a87981
Prateek Shukla <shuklaprateek@google.com>
2023-09-19 22:26:18 +0530

    npm explored

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,20 +1,41 @@
 const fs = require('fs');
 const http = require('http');
-
+const t1 = performance.now();
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
 
-    fs.readFile('index.cpp', (err, data) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end('Error reading file');
-        } else {
-            res.end(data);
-        }
-    });
+    // console.log(req.headers);
+    console.log(req.method);
+    switch (req.url) {
+        case "/users":
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(fs.readFileSync('user.json', 'utf-8'));
+            break;
+
+        case "/":
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(fs.readFileSync('index.html', 'utf-8'));
+            break;
+        default:
+            res.statusCode = 404;
+            res.end("Page Not Found!");
+    }
+
 });
+
+console.log(performance.now() - t1);
 
 server.listen(3000, 'localhost', () => {
     console.log('Server running at http://localhost:3000/');
 });
+
+
+// fs.readFile('index.txt', (err, data) => {
+//     if (err) {
+//         res.statusCode = 500;
+//         res.end('Error reading file');
+//     } else {
+//         res.end(data);
+//     }
+// });
